8c561c35165f6eaa7c4339c423aabfec6e636f07
Hanno Wiegard <hanno.wiegard@gmail.com>
2021-04-08 00:06:54 +0200

    Added the alias definitions to match the bash ones (minus some pretty print stuff which is outdated)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,18 @@
 set SPACEFISH_KUBECONTEXT_SHOW false
+
+alias ll='ls -alF'
+alias curltime="curl -w \"@$HOME/.curl-format.txt\" -o NUL -s "
+
+## dcu == Docker-Compose Up
+alias dcu='CURRENT_UID=(id -u):(id -g) docker-compose up -d --remove-orphans'
+
+## dcl == Docker-Compose Logs
+alias dcl='docker-compose logs'
+
+## dcs == Docker-Compose Stop
+alias dcs='docker-compose stop'
+
+## dcp == Docker-Compose process status
+alias dcp='docker-compose ps'
+
+alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
